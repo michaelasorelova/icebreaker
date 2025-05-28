bea2c4ef705f30c3f0485899b3e884868298182a
Michaela Šorelová <michaelasorelova@gmail.com>
2025-05-28 22:21:39 +0200

    extract SidebarMenu from Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { SidebarMenu } from "../SidebarMenu";
 import './style.css';
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
-    document.body.classList.toggle('no-scroll', isMenuOpen);
+    document.body.classList.toggle("no-scroll", isMenuOpen);
   }, [isMenuOpen]);
+
+  const closeMenu = () => setIsMenuOpen(false);
 
   return (
     <header className="header">
       <nav className="nav">
         <a className="nav__title" href="#">Icebreaker</a>
 
-        <div className={`nav__menu ${isMenuOpen ? 'overlay' : ''}`}>
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a className="nav__link" href="#">Domů</a>
-            </li>
-            <li className="nav__item">
-              <a className="nav__link" href="#">Oblíbené otázky</a>
-            </li>
-            <li className="nav__item">
-              <a className="nav__link" href="#">O projektu</a>
-            </li>
-            <li className="nav__item">
-              <a className="nav__link" href="#">Kontakt</a>
-            </li>
-          </ul>
-        </div>
+        <SidebarMenu isMenuOpen={isMenuOpen} closeMenu={closeMenu} />
 
         <button
           className="nav__toggle"
--- src/components/SidebarMenu/index.jsx
+++ src/components/SidebarMenu/index.jsx
@@ -1,1 +1,30 @@
+import React from "react";
 import './style.css';
+
+export const SidebarMenu = ({ isMenuOpen, closeMenu }) => {
+  return (
+    <>
+      <div className={`nav__menu ${isMenuOpen ? "overlay" : ""}`}>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a className="nav__link" href="#" onClick={closeMenu}>Domů</a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#" onClick={closeMenu}>Oblíbené otázky</a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#" onClick={closeMenu}>O projektu</a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#" onClick={closeMenu}>Kontakt</a>
+          </li>
+        </ul>
+      </div>
+
+      <div
+        className={`nav__backdrop ${isMenuOpen ? "is-active" : ""}`}
+        onClick={closeMenu}
+      ></div>
+    </>
+  );
+};
