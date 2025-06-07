fef758c88d57bd712265cfffb28d67c73ce9a108
Simona Šusterová <simsusterova@gmail.com>
2025-06-07 12:53:27 +0200

    palec

--- src/components/QuestionCard/index.jsx
+++ src/components/QuestionCard/index.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 import './style.css';
-import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export const QuestionCard = ({ OnSelectQuestion }) => {
   const [question, setQuestion] = useState(null); 
   const [selectedCategory, setSelectedCategory] = useState('na_rozehrati'); 
 
-const navigation = useNavigate();
-const state = navigation.state
-console.log(state);
+
+  const [liked, setLiked] = useState(false);
+const [disliked, setDisliked] = useState(false);
+
 
   useEffect(() => {
     const fetchQuestions = async () => {
@@ -36,29 +37,40 @@ console.log(state);
       }
     };
 
+
     fetchQuestions();
   }, []);
 
   if (!question) return <p>Načítání otázky…</p>;
 
-  return (
+   return (
     <div className="question-card">
       <div className="question-card__content">
         <p>{question.text}</p>
       </div>
       <div className="question-card__buttons">
         <button
-          className="question-card__button question-card__button--dislike"
+          className={`question-card__button question-card__button--dislike ${disliked ? 'active' : ''}`}
           aria-label="Nelíbí se mi"
+          onClick={() => {
+            setDisliked(!disliked);
+            if (liked) setLiked(false); 
+          }}
         >
-          <i className="fi fi-rr-hand"></i>
+          <i className="fi fi-sr-thumbs-down"></i>
         </button>
+
         <button
-          className="question-card__button question-card__button--like"
+          className={`question-card__button question-card__button--like ${liked ? 'active' : ''}`}
           aria-label="Líbí se mi"
+          onClick={() => {
+            setLiked(!liked);
+            if (disliked) setDisliked(false); 
+          }}
         >
-          <i className="fi fi-rr-social-network"></i>
+          <i className="fi fi-sr-thumbs-up"></i>
         </button>
+
         <button
           className="question-card__button question-card__button--save"
           aria-label="Přidat k oblíbeným"
