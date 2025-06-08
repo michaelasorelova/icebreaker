7cae897db17e6d3db87089d938a4039e9704f95b
Michaela Šorelová <michaelasorelova@gmail.com>
2025-06-08 16:31:08 +0200

    Update FavoriteQuestion

--- src/components/FavoriteQuestion/index.jsx
+++ src/components/FavoriteQuestion/index.jsx
@@ -7,14 +7,14 @@ export const FavoriteQuestion = ({ favorites, removeFavorite }) => {
         {favorites.length === 0 && <li>Nemáš zatím žádné oblíbené otázky.</li>}
         {favorites.map((favorite, index) => (
           <li key={index}>
-            <span>{favorite}</span>
             <button
               onClick={() => removeFavorite(index)}
-              className="icon-button text-red-500 hover:text-red-700 text-xl"
+              className="favorite-question__button"
               aria-label="Odebrat z oblíbených"
             >
               <i className="fi fi-sr-cross-circle"></i>
             </button>
+            <span>{favorite}</span>
           </li>
         ))}
       </ul>
