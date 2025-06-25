1b207c510b8d5f9570fefd400c3f4d2442cf1e8d
Michaela Šorelová <michaelasorelova@gmail.com>
2025-06-25 18:35:06 +0200

    Highlight active menu links with underline styling

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,25 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './style.css';
 
 export const Menu = ({ isOpen, onClose }) => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <>
       <nav className={`menu ${isOpen ? "menu--open" : ""}`}>
         <ul className="menu__list">
           <li className="menu__item">
-            <Link className="menu__link" to="/" onClick={onClose}>Domů</Link>
+            <Link
+              className={`menu__link ${currentPath === "/" ? "menu__link--active" : ""}`}
+              to="/"
+              onClick={onClose}
+            >
+              Domů
+            </Link>
           </li>
           <li className="menu__item">
-            <Link className="menu__link" to="/favorite-questions" onClick={onClose}>Oblíbené otázky</Link>
+            <Link
+              className={`menu__link ${currentPath === "/favorite-questions" ? "menu__link--active" : ""}`}
+              to="/favorite-questions"
+              onClick={onClose}
+            >
+              Oblíbené otázky
+            </Link>
           </li>
           <li className="menu__item">
-            <Link className="menu__link" to="/deleted-questions" onClick={onClose}>Smazané otázky</Link>
+            <Link
+              className={`menu__link ${currentPath === "/deleted-questions" ? "menu__link--active" : ""}`}
+              to="/deleted-questions"
+              onClick={onClose}
+            >
+              Smazané otázky
+            </Link>
           </li>
           <li className="menu__item">
-            <Link className="menu__link" to="/about" onClick={onClose}>O projektu</Link>
+            <Link
+              className={`menu__link ${currentPath === "/about" ? "menu__link--active" : ""}`}
+              to="/about"
+              onClick={onClose}
+            >
+              O projektu
+            </Link>
           </li>
           <li className="menu__item">
-            <Link className="menu__link" to="/contact" onClick={onClose}>Kontakt</Link>
+            <Link
+              className={`menu__link ${currentPath === "/contact" ? "menu__link--active" : ""}`}
+              to="/contact"
+              onClick={onClose}
+            >
+              Kontakt
+            </Link>
           </li>
         </ul>
       </nav>
